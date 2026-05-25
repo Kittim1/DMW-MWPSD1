7d0cee85209837c203a3712a6779a1b1a9212650
Kittim1 <kittim.ignalig8@gmail.com>
2026-05-25 16:33:36 +0800

    2nd week update

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -104,6 +104,7 @@ function Landing() {
               height="48"
             >
               <circle cx="12" cy="8" r="4" />
+
               <path d="M4 20c0-4 3.6-7 8-7s8 3 8 7" />
             </svg>
           </button>
